36d834f6d6d78afb606a8448e6d3256e57b831d4
erkins1 <35502998+erkins1@users.noreply.github.com>
2020-02-04 20:35:07 -0700

    Added Sandbox

---
+++ sandbox/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtblViewQuotes = new System.Windows.Forms.DataGridView();
+            this.cmbTest = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtblViewQuotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtblViewQuotes
+            // 
+            this.dtblViewQuotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtblViewQuotes.Location = new System.Drawing.Point(0, 120);
+            this.dtblViewQuotes.Name = "dtblViewQuotes";
+            this.dtblViewQuotes.Size = new System.Drawing.Size(801, 330);
+            this.dtblViewQuotes.TabIndex = 0;
+            // 
+            // cmbTest
+            // 
+            this.cmbTest.FormattingEnabled = true;
+            this.cmbTest.Location = new System.Drawing.Point(357, 28);
+            this.cmbTest.Name = "cmbTest";
+            this.cmbTest.Size = new System.Drawing.Size(221, 21);
+            this.cmbTest.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbTest);
+            this.Controls.Add(this.dtblViewQuotes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtblViewQuotes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtblViewQuotes;
+        private System.Windows.Forms.ComboBox cmbTest;
+    }
+}
+
---
+++ sandbox/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
+
+namespace sandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fillViewQuotes();
+            fileToArray();
+        }
+        
+        private void fillViewQuotes()
+        {
+
+            string json = File.ReadAllText("C:\\Users\\Geoffrey\\OneDrive\\BYU-I\\CIT 365 .NET Software\\NET-Software\\sandbox\\bin\\Debug\\Example.JSON");
+            //Console.WriteLine(json);
+
+            JArray exampleJS = JArray.Parse(json);
+
+            dtblViewQuotes.DataSource = exampleJS.Select(js => new
+            {
+                Color = (string)js["color"],
+                Category = (string)js["category"],
+                RGBA = (string)js["code"]["rgba"],
+                Hex = (string)js["code"]["hex"]
+            }).ToList();
+        }
+
+
+        private void fileToArray()
+        {
+
+            //int[,] prices = new int[2, 2];
+            /*int [][] readArray = File.ReadAllLines("C:\\Users\\Geoffrey\\OneDrive\\BYU-I\\CIT 365 .NET Software\\NET-Software\\sandbox\\bin\\Debug\\rushOrderPrices.txt")
+                .Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray())
+                   .ToArray();
+                   */
+
+            string[] file = File.ReadLines("C:\\Users\\Geoffrey\\OneDrive\\BYU-I\\CIT 365 .NET Software\\NET-Software\\sandbox\\bin\\Debug\\rushOrderPrices.txt").ToArray();
+
+            decimal[,] prices = new decimal[3,3];
+            int x = 0;
+            int y = 0;
+            for (int z = 0; z < file.Length; z++)
+            {
+                prices[y,x] = Convert.ToDecimal(file[z]);
+
+                x++;
+                if (x == 3)
+                {
+                    x = 0;
+                    y++;
+                }
+                    
+            }
+
+
+            Console.WriteLine(prices.ToString());
+
+        }
+
+
+
+
+        private void fillCMB()
+        {
+            List<CMBFill.testList> materialTypes =
+                Enum.GetValues(typeof(CMBFill.testList))
+                    .Cast<CMBFill.testList>().ToList();
+            //List<string> materialTypes = new CMBFill.testList;
+            cmbTest.DataSource = materialTypes;
+            cmbTest.SelectedIndex = -1;
+        }
+
+        public class CMBFill
+        {
+            public enum testList
+            {
+                aOak,
+                aLaminate,
+                aPine,
+                aRosewood,
+                aVeneer
+            }
+
+            public List<string> testLst = new List<string>
+        {
+            "carrot",
+            "fox",
+            "explorer"
+        };
+
+
+        }
+
+
+
+    }
+}
