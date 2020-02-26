68cf3fd4249da30ce1583936ba356ecce02f8511
Bhogge2 <hog15005@byui.edu>
2020-02-26 10:18:57 -0700

    quote scaffolding

---
+++ MegaDesk-Razor/Pages/Quotes/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MegaDesk_Razor.Data;
+using MegaDesk_Razor.Models;
+
+namespace MegaDesk_Razor
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MegaDesk_Razor.Data.MegaDesk_RazorContext _context;
+
+        public CreateModel(MegaDesk_Razor.Data.MegaDesk_RazorContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DeskQuote DeskQuote { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.DeskQuote.Add(DeskQuote);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ MegaDesk-Razor/Pages/Quotes/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MegaDesk_Razor.Data;
+using MegaDesk_Razor.Models;
+
+namespace MegaDesk_Razor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MegaDesk_Razor.Data.MegaDesk_RazorContext _context;
+
+        public IndexModel(MegaDesk_Razor.Data.MegaDesk_RazorContext context)
+        {
+            _context = context;
+        }
+
+        public IList<DeskQuote> DeskQuote { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            DeskQuote = await _context.DeskQuote.ToListAsync();
+        }
+    }
+}
