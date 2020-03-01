545348b85ce705421f91171b904ad7d3507eba01
erkins1 <35502998+erkins1@users.noreply.github.com>
2020-02-29 21:19:58 -0700

    Added Sort and Search features

--- MegaDesk-Razor/Pages/Quotes/Index.cshtml.cs
+++ MegaDesk-Razor/Pages/Quotes/Index.cshtml.cs
@@ -21,9 +21,46 @@ namespace MegaDesk_Razor
 
         public IList<DeskQuote> DeskQuote { get;set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        //[BindProperty(SupportsGet = true)]
+        //public string sortOrder { get; set; }
+
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            DeskQuote = await _context.DeskQuote.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "name";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            var quotes = from q in _context.DeskQuote select q;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    quotes = quotes.OrderByDescending(s => s.CustomerName);
+                    break;
+                case "Date":
+                    quotes = quotes.OrderBy(s => s.Date);
+                    break;
+                case "date_desc":
+                    quotes = quotes.OrderByDescending(s => s.Date);
+                    break;
+                case "name":
+                    quotes = quotes.OrderBy(s => s.CustomerName);
+                    break;
+            }
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                quotes = quotes.Where(q => q.CustomerName.Contains(searchString));
+            }
+
+            DeskQuote = await quotes.ToListAsync();
         }
     }
 }
