cdd2c655ed7518cf775be8ab591200667a0079aa
Geoffrey <rog13007@gmail.com>
2020-03-05 13:52:43 -0700

    Updated Seed DB

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -23,29 +23,55 @@ namespace MvcMovie.Models
                     return;   // DB has been seeded
                 }
 
+                
+                context.Genre.AddRange(
+                    new Genre
+                    {
+                        GenreName = "Comedy"
+                    },
+                    new Genre
+                    {
+                        GenreName = "Drama"
+                    },
+                    new Genre
+                    {
+                        GenreName = "Romanic Comedy"
+                    },
+                    new Genre
+                    {
+                        GenreName = "Documentary"
+                    },
+                    new Genre
+                    {
+                        GenreName = "Adventure"
+                    }
+
+                );
+                context.SaveChanges();
+
                 context.Movie.AddRange(
                     new Movie
                     {
-                        Title = "When Harry Met Sally",
-                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Title = "The Singles Ward",
+                        ReleaseDate = DateTime.Parse("2002-2-12"),
                         Genre = "Romantic Comedy",
                         Price = 7.99M,
-                        Rating = "R"
+                        Rating = "G"
                     },
 
                     new Movie
                     {
-                        Title = "Ghostbusters ",
-                        ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Genre = "Comedy",
+                        Title = "The Singles 2nd Ward",
+                        ReleaseDate = DateTime.Parse("2007-3-13"),
+                        Genre = "Romantic Comedy",
                         Price = 8.99M,
                         Rating = "PG"
                     },
 
                     new Movie
                     {
-                        Title = "Ghostbusters 2",
-                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Title = "Mobsters and Mormons",
+                        ReleaseDate = DateTime.Parse("2005-2-23"),
                         Genre = "Comedy",
                         Price = 9.99M,
                         Rating = "PG"
@@ -53,11 +79,20 @@ namespace MvcMovie.Models
 
                     new Movie
                     {
-                        Title = "Rio Bravo",
-                        ReleaseDate = DateTime.Parse("1959-4-15"),
-                        Genre = "Western",
-                        Price = 3.99M,
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2014-7-3"),
+                        Genre = "Documentary",
+                        Price = 9.99M,
                         Rating = "NR"
+                    },
+
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Drama",
+                        Price = 3.99M,
+                        Rating = "PG-13"
                     }
                 );
                 context.SaveChanges();
